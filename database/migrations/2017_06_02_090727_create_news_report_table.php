7fe98a765180fdbdd6fc39b9699acb142b370880
RifqiSec <rifqisec@gmail.com>
2017-06-02 16:09:42 +0700

    news report migrations

---
+++ database/migrations/2017_06_02_090727_create_news_report_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsReportTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news_report', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('news_id')->unsigned();
+            $table->integer('report_id')->unsigned();
+
+
+            $table->integer('news_id')->references('id')->on('news')->onDelete('cascade');
+            $table->integer('report_id')->references('id')->on('reports')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('news_report', function (Blueprint $table) {
+            //
+        });
+    }
+}
